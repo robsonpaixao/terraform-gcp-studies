ab91d8fca3c3a46c150da34384e76a3732f6685b
Robson Paixão <robson.emanuelpx@gmail.com>
2024-10-29 06:55:51 -0300

    creates storage-bucket-object using tf + add permissions of viewer to my user

--- gcs/main.tf
+++ gcs/main.tf
@@ -1,6 +1,28 @@
 resource "google_storage_bucket" "GCS1" {
   name          = "tf-course-bucker-from-terraform-robs"
   location      = "SOUTHAMERICA-EAST1"
-  storage_class = "STANDARD"
+  storage_class = "NEARLINE"
   force_destroy = true
+
+  labels = {
+    "env" = "tf_env"
+    "dep" = "complience"
+  }
+
+  uniform_bucket_level_access = true
 }
+
+resource "google_storage_bucket_object" "picture" {
+  name   = "troll-face"
+  bucket = google_storage_bucket.GCS1.name
+  source = "troll-face.webp"
+}
+
+resource "google_storage_bucket_iam_binding" "bucket_object_viewer" {
+  bucket = google_storage_bucket.GCS1.name
+  role   = "roles/storage.objectViewer"
+  members = [
+    "user:robson.emanuelpx@gmail.com"
+  ]
+}
+
