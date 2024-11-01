9bda40a069686bc915187bb535f18b62ab085012
Robson Paixão <robson.emanuelpx@gmail.com>
2024-11-01 06:59:12 -0300

    creates subnets from Terraform

--- network/main.tf
+++ network/main.tf
@@ -9,6 +9,15 @@ resource "google_compute_network" "custom-vpc-tf" {
   auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "sub-sg" {
+  name                     = "sub-sg"
+  network                  = google_compute_network.custom-vpc-tf.id
+  ip_cidr_range            = "10.1.0.0/24"
+  region                   = "asia-southeast1"
+  private_ip_google_access = true
+}
+
+
 output "auto" {
   value = google_compute_network.auto-vpc-tf.id
 }
