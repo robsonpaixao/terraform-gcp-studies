0b91461a6a6d9d2d1e032c9a2d7a60bb5fea05dc
Robson Paixão <robson.emanuelpx@gmail.com>
2024-11-01 06:50:21 -0300

    creates VPC from Terraform

---
+++ network/main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_network" "auto-vpc-tf" {
+  name                    = "auto-vpc-tf"
+  auto_create_subnetworks = true
+}
+
+
+resource "google_compute_network" "custom-vpc-tf" {
+  name                    = "custom-vpc-tf"
+  auto_create_subnetworks = false
+}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+output "custom" {
+  value = google_compute_network.custom-vpc-tf.id
+}
